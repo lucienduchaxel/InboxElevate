75b1538b39997140fa6b700c2ba19a77cfcfa21f
lucien du chaxel <lucien.duchaxel@gmail.com>
2025-03-29 18:24:05 -0700

    Update next config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    typescript: {
+        // Ignore type errors in the `src` directory
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        // Ignore lint errors in the `src` directory
+        ignoreDuringBuilds: true,
+    },
+};
 
 export default config;
